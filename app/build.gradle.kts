188ae04cf12bfce8092230f68a039c8c36dc6f44
Parveen Kumar <sudopk@google.com>
2021-10-03 16:55:33 -0700

    Upgrade versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,15 +3,15 @@ plugins {
   id("kotlin-android")
 }
 
-val composeVersion = "1.0.0-beta08"
+val composeVersion = "1.1.0-alpha05"
 
 android {
-  compileSdk = 30
+  compileSdk = 31
 
   defaultConfig {
     applicationId = "com.sudopk.counter"
     minSdk = 21
-    targetSdk = 30
+    targetSdk = compileSdk
     versionCode = 1
     versionName = "1.0"
 
@@ -43,19 +43,19 @@ android {
 }
 
 dependencies {
-  implementation("androidx.core:core-ktx:1.5.0")
+  implementation("androidx.core:core-ktx:1.6.0")
   implementation("androidx.preference:preference-ktx:1.1.1")
-  implementation("androidx.appcompat:appcompat:1.3.0")
-  implementation("com.google.android.material:material:1.3.0")
+  implementation("androidx.appcompat:appcompat:1.3.1")
+  implementation("com.google.android.material:material:1.4.0")
   implementation("androidx.compose.ui:ui:$composeVersion")
   implementation("androidx.compose.material:material:$composeVersion")
   implementation("androidx.compose.ui:ui-tooling:$composeVersion")
   implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
-  implementation("androidx.activity:activity-compose:1.3.0-beta01")
+  implementation("androidx.activity:activity-compose:1.4.0-beta01")
   implementation("com.github.sudopk:KAndroid:1.1.2")
 
   testImplementation("junit:junit:4.13.2")
-  androidTestImplementation("androidx.test.ext:junit:1.1.2")
-  androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
+  androidTestImplementation("androidx.test.ext:junit:1.1.3")
+  androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
   androidTestImplementation("androidx.compose.ui:ui-test-junit4:$composeVersion")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-  rootProject.extra["kotlinVersion"] = "1.5.10"
+  rootProject.extra["kotlinVersion"] = "1.5.31"
   val kotlinVersion = rootProject.extra["kotlinVersion"] as String
 
   repositories {
@@ -8,7 +8,7 @@ buildscript {
     mavenCentral()
   }
   dependencies {
-    classpath("com.android.tools.build:gradle:7.0.0-beta01")
+    classpath("com.android.tools.build:gradle:7.1.0-alpha12")
     classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
 
     // NOTE: Do not place your application dependencies here; they belong
